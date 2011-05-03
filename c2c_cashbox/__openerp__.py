b5e148637a068f5bcfc9e75f98bf373c4f677a9c
unknown <openerp@bazaar.camptocamp.net>
2011-05-03 17:46:08 +0200

    [FIX] chasbox - add date field to form

---
+++ c2c_cashbox/__openerp__.py
@@ -0,0 +1,18 @@
+# -*- coding: utf-8 -*-
+{
+     "name"         : "Cashbox add date field in form",
+     "version"      : "1.0",
+     "author"       : "Camptocamp Austria",
+     "website"      : "http://www.camptocamp.at",
+     "description"  : """
+     Adds date of cashbox to form to allow modifications
+     """,
+     "category"     : "Generic Modules/Accounting",
+     "depends"      : ["base","account"],
+     "init_xml"     : [],
+     "demo_xml"     : [],
+     "update_xml"   : ["cashbox_view.xml"],
+     "active"       : False,
+     "installable"  : True
+}
+
