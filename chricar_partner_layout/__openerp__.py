c18a69a0d70d9599d531b179ce289946ce637b8c
openerp@bazaar.camptocamp.net <openerp@bazaar.camptocamp.net>
2011-05-25 17:06:24 +0200

    [ADD] partner form layout to display long filenames

---
+++ chricar_partner_layout/__openerp__.py
@@ -0,0 +1,18 @@
+# -*- coding: utf-8 -*-
+{
+     "name"         : "Partner: better layout for lang partner names",
+     "version"      : "1.0",
+     "author"       : "Camptocamp Austria",
+     "website"      : "http://www.camptocamp.at",
+     "description"  : """
+     layout modification for partner name
+     """,
+     "category"     : "Generic Modules/Base",
+     "depends"      : ["base"],
+     "init_xml"     : [],
+     "demo_xml"     : [],
+     "update_xml"   : ["partner_view.xml"],
+     "active"       : False,
+     "installable"  : True
+}
+
