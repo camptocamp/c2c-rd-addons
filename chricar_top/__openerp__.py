ef00c11cd12928db4f0b5c1174c2c6a76e75a41b
openerp@bazaar.camptocamp.net <openerp@bazaar.camptocamp.net>
2011-11-10 17:37:11 +0100

    [FIX] missing dependency

--- chricar_top/__openerp__.py
+++ chricar_top/__openerp__.py
@@ -7,7 +7,7 @@
      "description"  : """defines the top (inherits location)
 generated 2009-07-09 16:17:22+02""",
      "category"     : "Client Modules/Real Estate",
-     "depends"      : ["base","product","stock","chricar_partner_parent_companies",],
+     "depends"      : ["base","product","stock","chricar_partner_parent_companies","crm_helpdesk"],
      "init_xml"     : ["mig_top_init.xml"],
      "demo_xml"     : [],
      "update_xml"   : ["top_view.xml","location_income_tax_view.xml","report_webkit.xml","security/ir.model.access.csv"],
