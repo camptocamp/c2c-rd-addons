eb3ecba6ec7e789521ca68cf8c1b826c4c9fd165
unknown <office@chricar>
2011-11-20 14:18:29 +0100

    [REF] deloitte

--- chricar_account_move_line_deloitte/account_move_line_deloitte.py
+++ chricar_account_move_line_deloitte/account_move_line_deloitte.py
@@ -1,6 +1,5 @@
-# -*- coding: utf-8 -*-
-
 #!/usr/bin/python
+# -*- coding: utf-8 -*-
 # -*- encoding: utf-8 -*-
 ##############################################
 #
@@ -32,6 +31,8 @@
 # 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.
 #
 ###############################################
+from time import mktime
+from datetime import datetime
 import time
 from osv import fields,osv
 import pooler
@@ -82,15 +83,53 @@ chricar_account_opening_deloitte()
 class chricar_account_move_line_deloitte(osv.osv):
      _name = "chricar.account_move_line_deloitte"
 
+     def _analytic_account_id(self, cr, uid, ids, name, arg, context):
+         result = {}
+         for move in self.browse(cr, uid, ids):
+             result[move.id] = False
+             if move.analytic_account and move.company_id:
+                 account_ids= self.pool.get('account.analytic.account').search(cr,uid,[('company_id','=',move.company_id.id),('code','=',move.analytic_account)])
+                 if len(account_ids):
+                     result[move.id] = account_ids[0]
+         return result
+
+     
+     def _period_id(self, cr, uid, ids, name, arg, context):
+         result = {}
+         for move in self.browse(cr, uid, ids):
+             date = False
+             try:
+                 date = time.strptime(move.date,'%d.%m.%y')
+             except:
+                 try:
+                    date = time.strptime(move.date,'%d/%m/%y')
+                 except:
+                    try:
+                       date = time.strptime(move.date,'%d-%m-%y')
+                    except:
+                       continue
+             date = datetime.fromtimestamp(mktime(date))
+             if date: 
+                 period_ids= self.pool.get('account.period').search(cr,uid,[('company_id','=',move.company_id.id),('date_start','<=',date),('date_stop','>=',date )])
+
+             if len(period_ids):
+                 result[move.id] = period_ids[0]
+
+         return result
+
      _columns = {
        'company_id'         : fields.many2one('res.company', 'Company'),
        'account'            : fields.char    ('Account', size=8, required=True),
        'amount'             : fields.float   ('Amount', required=True, digits=(16,2)),
-       'analytic_account'   : fields.char    ('Analytic Account', size=8),
+       'analytic_account'   : fields.char    ('Analytic Account Deloitte', size=8),
+       'analytic_account_id': fields.function(_analytic_account_id, method=True, string="Analytic Account",type='many2one', relation='account.analytic.account',  select="1", store=True ),
+
        'ba'                 : fields.char    ('BA', size=8),
        'bc'                 : fields.char    ('BC', size=8),
        'counter_account'    : fields.char    ('Counter Account', size=8),
        'date'               : fields.char    ('Date', size=16, required=True),
+       'period_id'          : fields.function(_period_id, method=True, string="Period",type='many2one', relation='account.period', store=True, select="1",  ),
+       'fiscalyear_id'      : fields.related ('period_id', 'fiscalyear_id', string='Fiscal Year', type='many2one', relation='account.fiscalyear', store=True),
        'description'        : fields.char    ('text', size=128),
        'fix'                : fields.char    ('Fix', size=8),
        'fnr'                : fields.char    ('FNR', size=8),
