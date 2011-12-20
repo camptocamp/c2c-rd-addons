ada0ef55a543c03d794a8606a13d009ec8bb3b10
Gerhard Könighofer <gerhard.koenighofer@swing-system.com>
2011-12-20 15:17:44 +0100

    c2c_sequence_fy: V6.1 ready

---
+++ c2c_sequence_fy/ir_sequence_installer.py
@@ -0,0 +1,38 @@
+# -*- coding: utf-8 -*-
+##############################################################################
+#
+#    OpenERP, Open Source Management Solution
+#    Copyright (C) 2004-2010 Tiny SPRL (<http://tiny.be>).
+#    Copyright (C) 2010-2010 Camptocamp Austria (<http://www.camptocamp.at>)
+#    Copyright (C) 2011-2011 Swing Entwicklung betrieblicher Informationssysteme GmbH (<http://www.swing-system.com>)
+#
+#    This program is free software: you can redistribute it and/or modify
+#    it under the terms of the GNU Affero General Public License as
+#    published by the Free Software Foundation, either version 3 of the
+#    License, or (at your option) any later version.
+#
+#    This program is distributed in the hope that it will be useful,
+#    but WITHOUT ANY WARRANTY; without even the implied warranty of
+#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#    GNU Affero General Public License for more details.
+#
+#    You should have received a copy of the GNU Affero General Public License
+#    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+#
+##############################################################################
+from osv import fields, osv
+
+class ir_sequence_installer(osv.osv_memory):
+    _name = 'ir.sequence.installer'
+    _inherit = 'res.config.installer'
+
+    def execute(self, cr, uid, ids, context=None):
+        cr.execute \
+            ("""UPDATE ir_sequence
+                 SET prefix = replace(prefix, '(year)', '(fy)'),
+                     suffix = replace(suffix, '(year)', '(fy)');"""
+            )
+        super(ir_sequence_installer, self).execute(cr, uid, ids, context=context)
+    # end def execute
+    
+ir_sequence_installer()
