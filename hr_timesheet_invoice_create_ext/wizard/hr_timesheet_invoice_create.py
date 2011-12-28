f54d1909bab204fb8274372152f7d603f317fa0c
unknown <office@chricar>
2011-12-28 23:03:39 +0100

    [FIX] hr_timesheet extension

--- hr_timesheet_invoice_create_ext/wizard/hr_timesheet_invoice_create.py
+++ hr_timesheet_invoice_create_ext/wizard/hr_timesheet_invoice_create.py
@@ -22,6 +22,7 @@ import time
 import datetime
 from osv import osv, fields
 from tools.translate import _
+import sys
 #
 # TODO: check unit of measure !!!
 #
@@ -38,6 +39,11 @@ class hr_timesheet_invoice_create(osv.osv_memory):
             , size=16
             , help='This text will be placed before the name of the analytic account instead of the current date'
             )
+        , 'journal_id'    : fields.many2one
+            ( 'account.journal'
+            , 'Journal'
+            , help='The journal to be used'
+            )
         , 'reference'    : fields.char
             ( 'Reference'
             , size=64
@@ -47,41 +53,61 @@ class hr_timesheet_invoice_create(osv.osv_memory):
     _defaults = {'reference' : lambda *a: 'automatic'}
     
     def _ref(self, dates) :
-        _min = datetime.datetime.strptime(dates[0][0:19], '%Y-%m-%d %H:%M:%S') 
-        _max = datetime.datetime.strptime(dates[-1][0:19], '%Y-%m-%d %H:%M:%S')
+        print >>sys.stderr,'dates ',dates
+        _min = datetime.datetime.strptime(dates[0][0:10], '%Y-%m-%d') 
+        _max = datetime.datetime.strptime(dates[-1][0:10], '%Y-%m-%d')
         if _min.year != _max.year :
-            return _('Output period: ') + dates[0] + ".." + dates[-1]
+            return _('Clearing period: ') + dates[0] + ".." + dates[-1]
         else :
             if _min.month == _max.month :
-                return _('Output period: ') + datetime.datetime.strftime(_min,"%b") + " " + str(_min.year)
+                return _('Clearing period: ') + datetime.datetime.strftime(_min,"%b") + " " + str(_min.year)
             else :
                 for i, r in enumerate([range(1,4), range(4,7), range(7,10), range(10,13)]) :
                     if (_min.month in r) and (_max.month in r) :
-                        return _('Output period: ') + str(i+1) + _(".Quarter ") + str(_min.year)
+                        return _('Clearing period: ') + str(i+1) + _(".Quarter ") + str(_min.year)
                 for i, r in enumerate([range(1,7), range(6,13)]) :
                     if (_min.month in r) and (_max.month in r) :
-                        return _('Output period: ') + str(i+1) + _(".Half-year ") + str(_min.year)
-                return _('Output period: ') + _("Calendaryear ") + str(_min.year)
+                        return _('Clearing period: ') + str(i+1) + _(".Half-year ") + str(_min.year)
+                return _('Clearing period: ') + _("Calendaryear ") + str(_min.year)
     # end def _ref
 
     def do_create(self, cr, uid, ids, context=None) :
         act_win = super(hr_timesheet_invoice_create, self).do_create(cr, uid, ids, context)
+        print >> sys.stderr, 'act_win A', act_win
         data = self.read(cr, uid, ids, [], context=context)[0]
         line_obj = self.pool.get('account.analytic.line')
         inv_obj  = self.pool.get('account.invoice')
 
-        inv_ids = line_obj.invoice_cost_create(cr, uid, context['active_ids'], data, context=context)
+        #inv_ids = line_obj.invoice_cost_create(cr, uid, context['active_ids'], data, context=context)
+        dom = act_win.get('domain')
+        print >> sys.stderr, 'dom', dom
+        # FIXME - dom [('id', 'in', [1512L]), ('type', '=', 'out_invoice')]
+        # wie findet man das 'id' - hier [0], könnte aber auch in anderer Reihenfolge kommen
+        inv_ids = dom[0][2] 
+ 
+        print >> sys.stderr, 'inv_ids', inv_ids
         for inv in inv_obj.browse(cr, uid, inv_ids) :
+            print >> sys.stderr,'inv',inv
+            print >> sys.stderr,'inv', inv.invoice_line
             if data['reference'] == 'automatic' :
-                ref = self._ref(sorted([l.account_analytic_id.date for l in inv.invoice_line]))
+                analytic_ids = line_obj.search(cr, uid, [('invoice_id','=',inv.id)])
+                lines = line_obj.browse(cr, uid, analytic_ids)
+                print >> sys.stderr,'analytic_ids', analytic_ids
+                #ref = self._ref(sorted([l.account_analytic_id.date for l in inv.invoice_line]))
+                ref = self._ref(sorted([l.date for l in lines]))
             else :
                 ref = data['reference'] or False
             values = \
-                { 'name'         : data['description'] + ' - ' + inv.invoice_line[0].account_analytic_id.name
-                , 'date_invoice' : data['date_invoice'] or False
+                { 'date_invoice' : data['date_invoice'] or False
                 , 'reference'    : ref
                 }
+            if data['description']:
+                values.update({ 'name'         : data['description'] + ' - ' + inv.invoice_line[0].account_analytic_id.name })
+            if data['journal_id']:
+                values.update({ 'journal_id'   : data['journal_id'][0] })
+            print >> sys.stderr,'values',inv.id ,values
             inv_obj.write(cr, uid, [inv.id], values)
+        print >> sys.stderr, 'act_win B', act_win
         return act_win
     # end def do_create
 hr_timesheet_invoice_create()
