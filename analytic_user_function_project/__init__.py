14adf0cf0bd71488c914c6782f3e1035478f73b2
openerp@bazaar.camptocamp.net <openerp@bazaar.camptocamp.net>
2011-12-29 14:25:03 +0100

    [FIX] missing

--- analytic_user_function_project/__init__.py
+++ analytic_user_function_project/__init__.py
@@ -19,7 +19,6 @@
 #    along with this program.  If not, see <http://www.gnu.org/licenses/>.     
 #
 ##############################################################################
-import wizard
+import hr_timesheet
 
-hr_timesheet
 # vim:expandtab:smartindent:tabstop=4:softtabstop=4:shiftwidth=4:
