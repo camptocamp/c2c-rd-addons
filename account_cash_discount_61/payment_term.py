0f039bec4c20f5a6be31f1a404256d68d77fa70c
unknown <office@chricar>
2012-01-15 18:35:27 +0100

    [REF] cash_discount 61 adapt payment term

--- account_cash_discount_61/payment_term.py
+++ account_cash_discount_61/payment_term.py
@@ -4,7 +4,7 @@ from osv import fields, osv
 
 
 class account_payment_term(osv.osv):
-    _name = "account.payment.term"
+    _inherit = "account.payment.term"
     _columns = {
          'is_discount': fields.boolean('Is Cash Discount', help="Check this box if this payment term is a cash discount. If cash discount is used the remaining amount of the invoice will not be paid")
     }
@@ -12,15 +12,21 @@ account_payment_term()
     
     
 class account_payment_term_line(osv.osv):
-    _name = "account.payment.term.line"
+    _inherit = "account.payment.term.line"
 
     _columns = {
-        'is_discount': fields.related('paymnet_id','is_discount', type='boolean', string='Is Cash Discount', readonly=True) ,
-        'discount': fields.float('Discount (%)', digits_compute=dp.get_precision('Account'), required=True),
-        'discount_income_account_id': fields.property('account.account', type='many2one', relation='account.account', string='Discount Invome Account', required=True, view_load=True,
+        'is_discount': fields.related('payment_id','is_discount', type='boolean', string='Is Cash Discount', readonly=True) ,
+        'discount': fields.float('Discount (%)', digits=(4,2), ),
+        'discount_income_account_id': fields.property('account.account', type='many2one', relation='account.account', string='Discount Income Account',  view_load=True,
               help="This account will be used to post the cash discount income"),
-        'discount_expense_account_id': fields.property('account.account', type='many2one', relation='account.account', string='Discount Expense Account', required=True, view_load=True,
+        'discount_expense_account_id': fields.property('account.account', type='many2one', relation='account.account', string='Discount Expense Account',  view_load=True,
               help="This account will be used to post the cash discount expense"),
     }    
+
+    def onchange_discount(self, cr, uid, ids, discount):
+        if not discount: return {}
+        result = {}
+        result = {'value': { 'value_amount': round(1-(discount/100.0),2), }}
+        return result
         
 account_payment_term_line()
