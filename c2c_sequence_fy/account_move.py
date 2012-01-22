4cd6972270cb95a4756cfa5c2fdff5f0ba8e99c3
unknown <office@chricar>
2012-01-22 10:35:22 +0100

    [FIX] coding - c2c_sequence tested seccessfully in v61 rc1

--- c2c_sequence_fy/account_move.py
+++ c2c_sequence_fy/account_move.py
@@ -1,4 +1,5 @@
 # -*- coding: utf-8 -*-
+# -*- coding: utf-8 -*-
 ##############################################################################
 #
 #    OpenERP, Open Source Management Solution
@@ -28,8 +29,11 @@ class account_move(osv.osv):
     def post(self, cr, uid, ids, context=None):
         import sys
         print >> sys.stderr,'post move context',context 
-
         journal_id = context.get('journal_id')
+        period_id = []
+        if 'period_id' in context:
+           period_id = [context.get('period_id')]
+        print >> sys.stderr,'post move period_id', period_id
         invoice_obj = context.get('invoice')
         if not journal_id:
            journal_id = invoice_obj.journal_id.id
@@ -55,8 +59,7 @@ class account_move(osv.osv):
             if jour.create_sequence == 'create_fy' :  
                 fy_seq_obj = self.pool.get('account.sequence.fiscalyear')
                 period_obj = self.pool.get('account.period')
-                period_ids = context.get('period_id')
-                if not period_ids:
+                if not period_id:
                    print >> sys.stderr,'per_id A'
                    period_id = invoice_obj.period_id.id 
                    print >> sys.stderr,'per_id B', period_id
@@ -66,9 +69,10 @@ class account_move(osv.osv):
                    print >> sys.stderr,'per_id D', period_id
                    
                 for period in period_obj.browse(cr, uid, period_id):
-                   fy_id = period.fiscalyear_id.id
-                   fy_code =  period.fiscalyear_id.code
-                   print >> sys.stderr,'fy_id', fy_id
+                    print >> sys.stderr,'fy_id', period
+                    fy_id = period.fiscalyear_id.id
+                    fy_code =  period.fiscalyear_id.code
+                    print >> sys.stderr,'fy_id a', fy_id
                 fy_seq = fy_seq_obj.search(cr, uid, [('fiscalyear_id','=', fy_id),('sequence_main_id','=',main_seq_id)])
                 print >> sys.stderr,'fy_seq_id', fy_seq
                 if not fy_seq:
@@ -88,47 +92,8 @@ class account_move(osv.osv):
                           }   
                    print >> sys.stderr,'fy_rel',fy_rel, prefix
                    fy_seq_obj.create(cr, uid, fy_rel)
-         
-        return super(account_move, self).post(cr, uid, ids, context)
+          #return True
         
-
-    def post_gk(self, cr, uid, ids, context=None):
-        if context is None : context = {}
-        invoice = context.get('invoice', False)
-        valid_move_ids = self.validate(cr, uid, ids, context)
-        if not valid_move_ids:
-            raise osv.except_osv \
-                ( _('Integrity Error !')
-                , _('You cannot validate a non-balanced entry!\nMake sure you have configured Payment Term properly !\nIt should contain at least one Payment Term Line with type "Balance" !')
-                )
-        seq_obj = self.pool.get('ir.sequence')
-        jou_obj = self.pool.get('account.journal')
-        for move in self.browse(cr, uid, valid_move_ids, context=context):
-            if move.name == '/':
-                new_name = False
-                journal = move.journal_id
-                if invoice and invoice.internal_number:
-                    new_name = invoice.internal_number
-                else:
-                    if journal.sequence_id:
-                        c = \
-                            { 'fiscalyear_id' : move.period_id.fiscalyear_id.id
-                            , 'journal_id'    : journal.id
-                            }
-                        new_name = seq_obj.next_by_id(cr, uid, journal.sequence_id.id, context=c)
-                    else :
-                        values = \
-                            { 'name'           : move.journal_id.name
-                            , 'prefix'         : "".join(w[0] for w in _(journal.name).split(' '))
-                            , 'padding'        : 3
-                            , 'implementation' : 'no_gap'
-                            }
-                        new_id   = seq_obj.create(cr, uid, values)
-                        new_name = seq_obj._format(self, cr, uid, seq_obj.browse(cr, uid, new_id), context)
-                        jou_obj.write(cr, uid, [journal_id.id], {'sequence_id' : new_id})
-                self.write(cr, uid, [move.id], {'name' : new_name})
-        self.write(cr, uid, valid_move_ids, {'state' : 'posted'})
-        return True
-    # end def post
+        return super(account_move, self).post(cr, uid, ids, context)
 
 account_move()
