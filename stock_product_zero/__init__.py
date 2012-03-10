60b5321ad0ccca4727697af82cbc16503db69850
unknown <office@chricar>
2012-03-10 22:55:56 +0100

    [REF] iexclude zero for inventory

--- stock_product_zero/__init__.py
+++ stock_product_zero/__init__.py
@@ -22,5 +22,6 @@
 
 
 import stock
+import inventory
 
 # vim:expandtab:smartindent:tabstop=4:softtabstop=4:shiftwidth=4:
---
+++ stock_product_zero/inventory.py
@@ -0,0 +1,60 @@
+# -*- coding: utf-8 -*-
+##############################################################################
+#
+#    OpenERP, Open Source Management Solution
+#    Copyright (C) 2004-2010 Tiny SPRL (<http://tiny.be>).
+#    Copyright (C) 2010-2010 Camptocamp Austria (<http://www.camptocamp.at>)
+#
+#    This program is free software: you can redistribute it and/or modify
+#    it under the terms of the GNU Affero General Public License as
+#    published by the Free Software Foundation, either version 3 of the
+#    License, or (at your option) any later version.
+#
+#    This program is distributed in the hope that it will be useful,
+#    but WITHOUT ANY WARRANTY; without even the implied warranty of
+#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#    GNU Affero General Public License for more details.
+#
+#    You should have received a copy of the GNU Affero General Public License
+#    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+#
+##############################################################################
+
+
+from osv import fields, osv
+import decimal_precision as dp
+import logging
+_logger = logging.getLogger(__name__)
+
+class stock_fill_inventory(osv.osv_memory):
+    _inherit = "stock.fill.inventory"
+    _columns = {
+          'display_with_zero_qty' : fields.boolean('Display lines with zero'),
+    }
+
+    def view_init(self, cr, uid, fields_list, context=None):
+        super(stock_fill_inventory, self).view_init(cr, uid, fields_list, context=context)
+        return True
+
+
+    def fill_inventory(self, cr, uid, ids, context=None):
+        #unfortunately not hook
+        inventory_id = context['id']
+        _logger = logging.getLogger(__name__)
+        _logger.info('FGF fill inventory ids, context %s, %s' % (ids,context))
+        display_with_zero_qty = None  
+        # FIXME - display_with_zero_qty access not possible
+        #fill_inventory = self.browse(cr, uid, ids, context=context)
+        #display_with_zero_qty = fill_inventory.display_with_zero_qty
+        
+        res_all = super(stock_fill_inventory, self).fill_inventory(cr, uid, ids, context)
+        
+        if not display_with_zero_qty:
+                inventory_line_obj = self.pool.get('stock.inventory.line')
+                ids_zero = inventory_line_obj.search(cr, uid, [('inventory_id','=', inventory_id), ('product_qty','=', '0')])
+                inventory_line_obj.unlink(cr, uid, ids_zero)
+
+        return res_all
+
+
+stock_fill_inventory()
