508e36bc07dca559e1ecdb6486e3d41590a50572
Gerhard Könighofer <gerhard.koenighofer@swing-system.com>
2012-04-16 09:39:38 +0200

    chricar_stock_dispo_production_V1: bug fix: wrong attributes

--- chricar_stock_dispo_production_V1/__openerp__.py
+++ chricar_stock_dispo_production_V1/__openerp__.py
@@ -1,16 +1,15 @@
-{
-     "name"         : "Dispo Production",
-     "version"      : "1.0",
-     "author"       : "ChriCar Beteiligungs- und Beratungs- GmbH",
-     "website"      : "http://www.chricar.at",
-     "description"  : """Dispo Produktion
-generated 2010-04-02 15:01:02+02""",
-     "category"     : "Client Modules/Farm",
-     "depends"      : ["base","sale","stock"],
-     "init_xml"     : ["mig_stock_dispo_production_init.xml"],
-     "demo_xml"     : [],
-     "update_xml"   : ["stock_dispo_production_view.xml","stock_prod_lot_update.xml"],
-     "active"       : False,
-     "installable"  : True
+{ "name"         : "Dispo Production"
+, "version"      : "1.0"
+, "author"       : "ChriCar Beteiligungs- und Beratungs- GmbH"
+, "website"      : "http://www.chricar.at"
+, "description"  : """Dispo Production
+generated 2010-04-02 15:01:02+02"""
+, "category"     : "Client Modules/Farm"
+, "depends"      : ["sale","stock"]
+, "init_xml"     : ["mig_stock_dispo_production_init.xml"]
+, "demo_xml"     : []
+, "update_xml"   : ["stock_dispo_production_view.xml","stock_prod_lot_update.xml"]
+, "active"       : False
+, "installable"  : True
 }
 
