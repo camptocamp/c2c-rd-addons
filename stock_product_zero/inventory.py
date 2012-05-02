670c26e7019bbf4e36adcc0dddb20876d72684d9
ferdinand <office@chricar.at>
2012-05-02 12:34:49 +0200

    [FIX] sort inventory by product name (without code)

--- stock_product_zero/inventory.py
+++ stock_product_zero/inventory.py
@@ -33,7 +33,8 @@ class stock_inventory(osv.osv):
 		, 'inventory_id'
 	        , 'Inventories'
 		, states={'done': [('readonly', True)]}
-		, order = 'location_id.name, product_id.name_template'	),
+		, order = 'product_id.name'	),
+
         #'move_ids': fields.many2many('stock.move', 'stock_inventory_move_rel', 'inventory_id', 'move_id', 'Created Moves'),
         #'move_ids': one2many_sorted.one2many_sorted('stock.move', 'stock_inventory_move_rel', 'inventory_id', 'move_id', 'Created Moves' , order = 'product_id.name',)
     }
