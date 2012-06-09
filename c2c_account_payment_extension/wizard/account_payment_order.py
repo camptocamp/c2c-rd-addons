2f2c89e448ce008f2e74552920fbf24a7d2edac6
Gerhard Könighofer <gerhard.koenighofer@swing-system.com>
2012-06-09 16:31:42 +0200

    c2c_account_payment_extension: rewrite

---
+++ c2c_account_payment_extension/wizard/account_payment_order.py
@@ -0,0 +1,99 @@
+# -*- coding: utf-8 -*-
+##############################################
+#
+# Swing Entwicklung betrieblicher Informationssysteme GmbH
+# (<http://www.swing-system.com>)
+# Copyright (C) ChriCar Beteiligungs- und Beratungs- GmbH
+# all rights reserved
+#    08-JUN-2012 (GK) created
+#
+# WARNING: This program as such is intended to be used by professional
+# programmers who take the whole responsibility of assessing all potential
+# consequences resulting from its eventual inadequacies and bugs.
+# End users who are looking for a ready-to-use solution with commercial
+# guarantees and support are strongly advised to contract a Free Software
+# Service Company.
+#
+# This program is Free Software; you can redistribute it and/or
+# modify it under the terms of the GNU General Public License
+# as published by the Free Software Foundation; either version 3
+# of the License, or (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program; if not, see <http://www.gnu.org/licenses/> or
+# write to the Free Software Foundation, Inc.,
+# 59 Temple Place - Suite 330, Boston, MA  02111-1.17, USA.
+#
+###############################################
+from osv import osv, fields
+
+class payment_order_create(osv.osv_memory):
+    _name        = 'payment.order.create.c2c'
+    _inherit     = 'payment.order.create'
+    _description = 'payment.order.create'
+    _columns     = \
+        { 'balance_filter' : fields.boolean
+            ( 'Only partners with total liability'
+            , help='Will select only partners with a debit-credit < 0 regardless of due date.'
+            )
+        , 'min_balance'    : fields.float 
+            ( 'Minimum Total Amount Due'
+            , help="Will select only partners with total payment balance greater than this amount."
+            )
+        }
+    _defaults    = \
+        { 'balance_filter' : lambda *a: True
+        , 'min_balance'    : lambda *a: 100.0
+        }
+
+    def search_entries(self, cr, uid, ids, context=None):
+        if context is None : context = {}
+        order_obj = self.pool.get('payment.order')
+        line_obj  = self.pool.get('account.move.line')
+        mod_obj   = self.pool.get('ir.model.data')
+        obj       = self.browse(cr, uid, ids, context=context)[0]
+        payment   = order_obj.browse(cr, uid, context['active_id'], context=context)
+        domain    = \
+            [ ('reconcile_id', '=', False)
+            , ('account_id.type', '=', 'payable')
+            , ('amount_to_pay', '>', 0)
+            ]
+        domain   += \
+            [ '|'
+            , ('date_maturity', '<=', obj.duedate)
+            , ('date_maturity', '=', False)
+            ]
+        ids = line_obj.search(cr, uid, domain, context=context)
+        line_ids = []
+        for line in line_obj.browse(cr, uid, ids) :
+            if line.invoice.payment_block : continue
+            if line.move_id.state == "draft" : continue
+            if not line.partner_bank_id and payment.mode.require_bank_account : continue
+            if line.partner_id.payment_block : continue
+            if (line.partner_id.payment_obey_balance 
+                and obj.balance_filter 
+                and not ((line.partner_id.debit - line.partner_id.credit) >= obj.min_balance)) : continue
+            line_ids.append(line.id)
+        context.update({'line_ids': line_ids})
+        model_data_ids = mod_obj.search \
+            ( cr, uid
+            , [('model', '=', 'ir.ui.view'), ('name', '=', 'view_create_payment_order_lines')]
+            , context=context
+            )
+        resource_id = mod_obj.read(cr, uid, model_data_ids, fields=['res_id'], context=context)[0]['res_id']
+        return \
+            { 'name'      : ('Entry Lines')
+            , 'context'   : context
+            , 'view_type' : 'form'
+            , 'view_mode' : 'form'
+            , 'res_model' : 'payment.order.create'
+            , 'views'     : [(resource_id,'form')]
+            , 'type'      : 'ir.actions.act_window'
+            , 'target'    : 'new'
+            }
+payment_order_create()
