f1bf1fc530fa81b1e313c536185110251b6577ca
ferdinand <office@chricar.at>
2012-08-22 17:07:36 +0200

    [ADD] account_fiscalyear_info

---
+++ account_fiscalyear_info/fiscalyear.py
@@ -0,0 +1,37 @@
+# -*- coding: utf-8 -*-
+##############################################################################
+#
+#    OpenERP, Open Source Management Solution
+#    Copyright (C) 2004-2011 Tiny SPRL (<http://tiny.be>).
+#    Copyright (C) 2011 Camptocamp Austria (<http://www.camptocamp.com>).
+#
+#    This program is free software: you can redistribute it and/or modify
+#    it under the terms of the GNU Affero General Public License as
+#    published by the Free Software Foundation, either version 3 of the
+#    License, or (at your option) any later version.
+#
+#    This program is distributed in the hope that it will be useful,
+#    but WITHOUT ANY WARRANTY; without even the implied warranty of
+#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#    GNU Affero General Public License for more details.
+#
+#    You should have received a copy of the GNU Affero General Public License
+#    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+#
+##############################################################################
+import decimal_precision as dp
+from osv import fields, osv, orm
+from tools.translate import _
+import one2many_sorted
+
+class account_fiscalyear(osv.osv):
+    _inherit = "account.fiscalyear"
+
+    _columns = {
+        'date_balance_resulution'   : fields.date("Balance Resolution")
+        , 'date_balance_file_reg'   : fields.date("Balance filed to Commercial Register")
+        , 'date_balance_publish'    : fields.date("Balance Publishing")
+        , 'date_tax_file'           : fields.date("Tax filed to Authorities")
+        , 'date_tax_assessed'        : fields.date("Tax assesed by Authorities")
+    }
+account_fiscalyear()
