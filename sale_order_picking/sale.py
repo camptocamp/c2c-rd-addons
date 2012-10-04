0b949db48069bef6e20dba810e9693f3e2ceb415
ferdinand <office@chricar.at>
2012-10-04 10:03:46 +0200

    [ADD] sale_order_picking

---
+++ sale_order_picking/sale.py
@@ -0,0 +1,75 @@
+# -*- coding: utf-8 -*-
+##############################################################################
+#
+#    OpenERP, Open Source Management Solution
+#    Copyright (C) 2004-2010 Tiny SPRL (<http://tiny.be>).
+#    Copyright (C) 2010-2012 Camptocamp Austria (<http://www.camptocamp.at>)
+#
+#    This program is free software: you can redistribute it and/or modify
+#    it under the terms of the GNU Affero General Public License as
+#    published by the Free Software Foundation, either version 3 of the
+#    License, or (at your option) any later version.
+#
+#    This program is distributed in the hope that it will be useful,
+#    but WITHOUT ANY WARRANTY; without even the implied warranty of
+#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#    GNU Affero General Public License for more details.
+#
+#    You should have received a copy of the GNU Affero General Public License
+#    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+#
+##############################################################################
+from osv import fields, osv
+#import logging
+
+class sale_order(osv.osv):
+    _inherit = "sale.order"
+
+    def _prepare_order_line_move(self, cr, uid, order, line, picking_id, date_planned, context=None):
+        """ This will automatically choose the oldest production lot and location with qty > 0
+        """
+        prodlot_obj = self.pool.get('stock.production.lot')
+        stock_location_obj = self.pool.get('stock.location')
+        
+        res = super(sale_order, self)._prepare_order_line_move(cr, uid, order, line, picking_id, date_planned, context)
+        
+        product_id = res['product_id']
+        lot_ids = prodlot_obj.search(cr, uid, [('product_id','=', product_id)])
+        lot_where = ' '
+        if lot_ids:        
+	    lot_where = ' and m.prodlot_id in ' + str(tuple(lot_ids)) + ' '
+        
+        # find stock location with 
+        top_location_id = order.shop_id.warehouse_id.lot_stock_id.id
+        location_ids = stock_location_obj.search(cr, uid, [('location_id','child_of', [top_location_id]) ], context=context)
+                             
+        
+        sql = """select l.id, m.prodlot_id,
+                 sum(case when l.id = m.location_dest_id then product_qty
+                      else -product_qty end) as qty_available
+                 from stock_move m,
+                      stock_location l
+                 where m.product_id = %s %s
+                   and m.state = 'done'
+                   and l.usage = 'internal'
+                   and l.id in  %s 
+                   and (m.location_id = l.id or m.location_dest_id = l.id) 
+                 group by l.id, m.prodlot_id
+                 having sum(case when l.id = m.location_dest_id then product_qty
+                      else -product_qty end) > 0
+                 order by m.prodlot_id asc
+               """ % (product_id, lot_where, tuple(location_ids))
+        cr.execute(sql)
+        locations = cr.dictfetchall()
+        
+        for r in locations:
+	    res['location_id'] = r['id']
+	    if r['prodlot_id']:
+               res['prodlot_id'] = r['prodlot_id']        
+            continue
+     
+               
+        return res
+        
+sale_order()
+
