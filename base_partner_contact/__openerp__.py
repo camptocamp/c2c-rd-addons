bfbea0216522fa58d41444367ecd6ab5b5631c9c
ferdinand <office@chricar.at>
2012-10-14 22:09:42 +0200

    [FIX] create, access rights

--- base_partner_contact/__openerp__.py
+++ base_partner_contact/__openerp__.py
@@ -31,7 +31,7 @@ This module allows to uses person as partners respecting first, middle, last nam
     "depends" : ["base"],
     "init_xml" : [ ],
     "demo_xml" : [ ],
-    "update_xml" : [ "partner_view.xml" ],
+    "update_xml" : [ "partner_view.xml", "security/ir.model.access.csv", ],
     "installable": True
 }
 # vim:expandtab:smartindent:tabstop=4:softtabstop=4:shiftwidth=4:
