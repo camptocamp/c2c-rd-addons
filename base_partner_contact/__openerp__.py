2949455b08e694382954ffe0c45434946d280241
ferdinand <office@chricar.at>
2012-10-14 17:55:16 +0200

    [ADD] base_partner_contact

---
+++ base_partner_contact/__openerp__.py
@@ -0,0 +1,38 @@
+# -*- encoding: utf-8 -*-
+##############################################################################
+#    
+#    OpenERP, Open Source Management Solution
+#    Copyright (C) 2004-2009 Tiny SPRL (<http://tiny.be>).
+#
+#    This program is free software: you can redistribute it and/or modify
+#    it under the terms of the GNU Affero General Public License as
+#    published by the Free Software Foundation, either version 3 of the
+#    License, or (at your option) any later version.
+#
+#    This program is distributed in the hope that it will be useful,
+#    but WITHOUT ANY WARRANTY; without even the implied warranty of
+#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#    GNU Affero General Public License for more details.
+#
+#    You should have received a copy of the GNU Affero General Public License
+#    along with this program.  If not, see <http://www.gnu.org/licenses/>.     
+#
+##############################################################################
+{
+    "name" : "Base Partner or Contact",
+    "version" : "1.0",
+    "author" : "Camptocamp Austria",
+    "website" : "http://www.camptocamp.com",
+    "category" : "Base",
+    "description": """
+This module allows to uses person as partners respecting first, middle, last name and title prefix and postfix.
+
+    """,
+    "depends" : ["base"],
+    "init_xml" : [ ],
+    "demo_xml" : [ ],
+    "update_xml" : [ "partner_view.xml" ],
+    "installable": True
+}
+# vim:expandtab:smartindent:tabstop=4:softtabstop=4:shiftwidth=4:
+
