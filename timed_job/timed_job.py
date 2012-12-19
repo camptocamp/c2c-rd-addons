37611a7d8f04bc3e211c961ee0164e44dcf4ac84
Gerhard Könighofer <gerhard.koenighofer@swing-system.com>
2012-12-19 07:42:46 +0100

    timed_job: improved error handling

--- timed_job/timed_job.py
+++ timed_job/timed_job.py
@@ -477,7 +477,7 @@ Depending on this interval unit the length of the interval can be specified:
     # end def _scheduler
 
     def _send_mail(cr, uid, job_id) :
-        self._logger.debug("_send_mail %s %s", uid, job_id) #######
+        self._logger.error("_send_mail %s %s", uid, job_id) #######
         name = "Notification Timed Job"
         mail_obj = self.pool.get("email.template")
         tpl_ids = mail_obj.search(cr, uid, [("name", "=", name)])
