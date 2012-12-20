0681fd007f2fe9f9734aa123b2b9c51493966a9b
ferdinand <office@chricar.at>
2012-12-20 08:09:11 +0100

    [FIX] - remove import wizard - no tested

--- report_crm_helpdesk/__init__.py
+++ report_crm_helpdesk/__init__.py
@@ -28,5 +28,5 @@
 # Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.
 #
 ##############################################################################
-import wizard
+#import wizard
 import report
