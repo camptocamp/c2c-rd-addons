e44314e5cde4e8bdfa91ea4432d041081f68df95
ferdinand <office@chricar.at>
2012-12-21 18:09:28 +0100

    [FIX] adapt to new demo data

--- table_generate_csv/wizard/wizard_generate_csv.py
+++ table_generate_csv/wizard/wizard_generate_csv.py
@@ -30,7 +30,7 @@
 # 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.
 #
 ###############################################
-import wizard
+#import wizard
 import pooler
 from osv import osv
 from tools.translate import _
