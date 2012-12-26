cf5be63abd28bbafd802872b7956f78d3ef84779
ferdinand <office@chricar.at>
2012-12-26 20:00:18 +0100

    [FIX] installable

--- c2c_stock_track_internal/__openerp__.py
+++ c2c_stock_track_internal/__openerp__.py
@@ -35,7 +35,7 @@ Requires production lot for internal moves
        ],
     #'update_xml': ['product_view.xml'],
     'demo_xml': [],
-    'installable': False,
+    'installable': True,
     'active': False,
 }
 # vim:expandtab:smartindent:tabstop=4:softtabstop=4:shiftwidth=4:
