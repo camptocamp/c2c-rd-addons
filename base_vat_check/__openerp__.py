ada36f9b40686c36e73a0afa4580d2cc2c407db5
ferdinand <office@chricar.at>
2013-06-05 10:30:00 +0200

    [REF] better text

--- base_vat_check/__openerp__.py
+++ base_vat_check/__openerp__.py
@@ -26,10 +26,9 @@
 This module enhances the VAT check to comply to accounting standards
 
 * save date and the method (vies/checksum) used for checking
+* automatically save the company name associated with the VAT-ID
+** at least some German VAT numbers return '---' as company name !!!
 
-ToDo - 
-
-* automatically check the current available methods do not check the company name associated with the VAT-ID
 """
 , 'author'      : 'Camptocamp Austria'
 , 'depends'     : [ 'base_vat' ]
