1a21890f67203b769c41d136d1d5c0670e39058f
ferdinand <office@chricar.at>
2013-06-20 18:11:13 +0200

    [FIX] take date form parent seq instead of journal

--- c2c_sequence_fy/ir_sequence.py
+++ c2c_sequence_fy/ir_sequence.py
@@ -109,19 +109,25 @@ class ir_sequence(osv.osv):
                     for fy_s in  fy_seq_obj.browse(cr, uid, fy_seq_ids):
                         seq_id = fy_s.sequence_id.id
                 else:
+                    for seq in selb.browse(cr, uid, sequence_id, context):
+                        sequence_code = seq.code
+                        sequence_name = seq.name
+                        sequence_padding = seq.padding
+                        sequence_implementation = seq.implementation
+                        prefix = seq.prefix or '' + fy.code +'-'
+                        company_id = seq.company_id.id
+                         
                     fy_obj = self.pool.get('account.fiscalyear')
                     for fy in fy_obj.browse(cr, uid, [fy]):
                         fy_code = fy.code
                         fy_name = fy.name
-                        prefix = journal.sequence_id.prefix or '' + fy.code +'-'
-                    sequence_code = journal.sequence_id.code
-                    company_id = journal.company_id.id
+
                     vals = \
                     { 'code'           : sequence_code
-                    , 'name'           : journal.sequence_id.name +' '+ fy_name
+                    , 'name'           : sequence_name +' '+ fy_name
                     , 'prefix'         : prefix
-                    , 'padding'        : journal.sequence_id.padding
-                    , 'implementation' : journal.sequence_id.implementation
+                    , 'padding'        : sequence_padding
+                    , 'implementation' : sequence_implementation
                     , 'company_id'     : company_id
                     }
                     seq_id = self.create(cr, uid, vals)
