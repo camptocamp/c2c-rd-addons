195fc5293a657c296895d3f57f3c8713aa043bbc
ferdinand <office@chricar.at>
2013-07-02 16:57:34 +0200

    [REF] select and group billing by partner

--- hr_timesheet_invoice_create_ext/hr_timesheet_invoice.py
+++ hr_timesheet_invoice_create_ext/hr_timesheet_invoice.py
@@ -1,9 +1,9 @@
 # -*- coding: utf-8 -*-
 ##############################################################################
-#    
+#
 #    OpenERP, Open Source Management Solution
-#    Copyright (C) 2004-2010 Tiny SPRL (<http://tiny.be>).
-#    Copyright (C) 2010-2012 Camptocamp Austria (<http://www.camptocamp.at>)
+#    Copyright (C) 2004-2011 Tiny SPRL (<http://tiny.be>).
+#    Copyright (C) 2013 Camptocamp Austria (<http://www.camptocamp.com>).
 #
 #    This program is free software: you can redistribute it and/or modify
 #    it under the terms of the GNU Affero General Public License as
@@ -16,9 +16,17 @@
 #    GNU Affero General Public License for more details.
 #
 #    You should have received a copy of the GNU Affero General Public License
-#    along with this program.  If not, see <http://www.gnu.org/licenses/>.     
+#    along with this program.  If not, see <http://www.gnu.org/licenses/>.
 #
 ##############################################################################
-import wizard
-import hr_timesheet_invoice
-# vim:expandtab:smartindent:tabstop=4:softtabstop=4:shiftwidth=4:
+
+from osv import fields
+from osv import osv
+
+class account_analytic_line(osv.osv):
+    _inherit = 'account.analytic.line'
+    _columns = {
+        'partner_id': fields.related('account_id', 'partner_id', type='many2one', string='Partner', relation='res.partner', store=True),
+    }
+
+account_analytic_line()
