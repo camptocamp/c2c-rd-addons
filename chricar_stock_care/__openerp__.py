b7a410633f643f5cc6fe36c17d160b0db66b5e8c
ferdinand <office@chrcar.at>
2013-08-10 08:14:03 +0200

    [FIX] bug 1194631

--- chricar_stock_care/__openerp__.py
+++ chricar_stock_care/__openerp__.py
@@ -3,10 +3,9 @@
 , "version"      : "1.0"
 , "author"       : "ChriCar Beteiligungs- und Beratungs- GmbH"
 , "website"      : "http://www.chricar.at"
-, "description"  : """Stock Care
-generated 2010-04-03 21:47:30+02"""
+, "description"  : """Stock Care"""
 , "category"     : "Client Modules/Farm"
-, "depends"      : ["product", "stock"]
+, "depends"      : ["product", "stock","c2c_product_price_unit"]
 , "init_xml"     : []
 , "demo_xml"     : []
 , "update_xml"   : ["stock_care_view.xml"]
