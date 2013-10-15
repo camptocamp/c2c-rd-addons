8051b674909e21e85a1c8b9087dcc8530168cd8d
ferdinand <office@chricar.at>
2013-10-15 15:02:54 +0200

    [ADD] account_invoice_change_date

---
+++ account_invoice_change_date/__openerp__.py
@@ -0,0 +1,39 @@
+# -*- coding: utf-8 -*-
+##############################################################################
+#
+#    OpenERP, Open Source Management Solution
+#    Copyright (C) 2004-2010 Tiny SPRL (<http://tiny.be>).
+#    Copyright (C) 2010-2012 ChriCar Beteiligung und Beratung GmbH (<http://www.chricar.at>)
+#
+#    This program is free software: you can redistribute it and/or modify
+#    it under the terms of the GNU Affero General Public License as
+#    published by the Free Software Foundation, either version 3 of the
+#    License, or (at your option) any later version.
+#
+#    This program is distributed in the hope that it will be useful,
+#    but WITHOUT ANY WARRANTY; without even the implied warranty of
+#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#    GNU Affero General Public License for more details.
+#
+#    You should have received a copy of the GNU Affero General Public License
+#    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+#
+##############################################################################
+
+
+{
+    'name': 'Invoice Change Date',
+    'version': '1.0',
+    'category': 'Others',
+    'description': """
+    Change date and period of an open/paid invoice
+    possible as long as periods are open
+""",
+    'author': 'ChriCar Beteiligung und Beratung GmbH',
+    'depends': ["account","chricar_account_period_sum"],
+    'update_xml': ['account_invoice_view.xml'],
+    'demo_xml': [],
+    'installable': True,
+    'active': False,
+}
+# vim:expandtab:smartindent:tabstop=4:softtabstop=4:shiftwidth=4:
