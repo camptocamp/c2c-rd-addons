924a3c2606341fa25af966901966372d516d0cda
ferdinand <office@chrcar.at>
2014-01-05 16:20:52 +0100

    [REF] user comp data

--- user_multi_comp/user.py
+++ user_multi_comp/user.py
@@ -29,18 +29,67 @@ class res_users_company(osv.osv):
     _columns = {
         'user_id': fields.many2one('res.users', 'User'),
         'company_id': fields.many2one('res.company', 'Company'),
-        'user_mail': fields.char('Email', size=240, help="Email for this company"),
+        'user_email': fields.char('Email', size=240, help="Email for this company"),
         'signature': fields.text('Signature', help="Signature for this company"),
         }
+
+    def init(self, cr):
+      return
+      cr.execute("""
+          insert into res_users_company(user_id,company_id,user_email,signature)
+                 select id,company_id,email,signature from res_config_users u
+                 where (u.id,u.company_id) not in (select user_id,company_id from res_users_company);
+      """)
+
     
 res_users_company()
 
 
-class hr_employee(osv.osv):
+class users(osv.osv):
     _inherit = "res.users"
+
+    def _get_email(self, cr, uid, ids, field, arg, context=None):
+        if not context:
+            context = {}
+        user_comp_obj = self.pool.get('res.users.company')
+        res = {}        
+        
+        c_id = ''
+        if context.get('company_id'):
+            c_id = context['company_id']
+        for user in self.browse(cr, uid, ids):         
+            res[user.id] = ''
+            for comp in user.user_company_ids:
+                if c_id or user.company_id.id == comp.company_id.id:
+                    #c_id = user.company_id.id
+                    res[user.id] = comp.user_email
+                
+        return res
+
+    def _get_signature(self, cr, uid, ids, field, arg, context=None):
+        if not context:
+            context = {}
+        user_comp_obj = self.pool.get('res.users.company')
+        res = {}        
+        
+        c_id = ''
+        if context.get('company_id'):
+            c_id = context['company_id']
+        for user in self.browse(cr, uid, ids):         
+            res[user.id] = ''
+
+            for comp in user.user_company_ids:
+                if c_id or user.company_id.id == comp.company_id.id:
+                    #c_id = user.company_id.id
+                    res[user.id] = comp.signature
+                
+        return res
+    
     _columns = {
-        'user_company_ids': one2many_sorted.one2many_sorted('res.users.company', 'user_id', 'Company Data' , order = 'company_id.name')    
-                                
+        'user_company_ids': one2many_sorted.one2many_sorted('res.users.company', 'user_id', 'Company Data' , order = 'company_id.name') ,   
+        'user_email': fields.function(_get_email, type='char', string='Email'),
+        'signature': fields.function(_get_signature, type='text', string='Signature'),
+      
         }
 
-hr_employee()
+users()
