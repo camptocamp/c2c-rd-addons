717cef1e5dd2b4056006d651f2811192c0950e23
Ivan Yelizariev <ivann.exe@gmail.com>
2015-03-31 14:50:48 +0200

    upload work from repo https://github.com/yelizariev/pos-addons

--- chricar_product_gtin/__openerp__.py
+++ chricar_product_gtin/__openerp__.py
@@ -1,18 +1,18 @@
 { 'sequence': 500,
 "name"         : "Product GTIN EAN8 EAN13 UPC JPC Support"
 , "version"      : "1.1"
-, "author"       : "ChriCar Beteiligungs- und Beratungs- GmbH"
+, "author"       : "Camptocamp, Ivan Yelizariev"
 , "website"      : "http://www.chricar.at/ChriCar"
 , "category"     : "Generic Modules/Others"
-, "depends"      : ["product"]
+, "depends"      : ["product", "point_of_sale"]
 , "description"  : """
 Replaces the EAN13 code completion with a checkroutine for EAN13, EAN8, JPC, UPC and GTIN
 makes EAN visible in simplified view
 """
 , "init_xml"     : []
 , "demo"         : []
-, "data"   : ["chricar_product_gtin_view.xml"]
+, "data"         : ["chricar_product_gtin_view.xml", "data.xml"]
 , "auto_install" : False
-, 'installable': False
 , 'application'  : False
+, "installable"  : True
 }
--- chricar_product_gtin/chricar_product_gtin.py
+++ chricar_product_gtin/chricar_product_gtin.py
@@ -20,10 +20,7 @@
 #
 ##############################################################################
 from openerp.osv import fields, osv
-import operator
-
-def is_pair(x):
-    return not x%2
+import math
 
 def check_ean(eancode):
      if not eancode:
@@ -34,22 +31,65 @@ def check_ean(eancode):
          int(eancode)
      except:
          return False
-     sum=0
-     ean13_len= int(len(eancode))
-     for i in range(ean13_len-1):
-         pos=int(ean13_len-2-i)
-         if is_pair(i): 
-             sum += 3 * int(eancode[pos])
-         else:
-             sum += int(eancode[pos])
-     check = 10 - operator.mod(sum,10)
-     if check == 10 :
-         check = 0
-     if check != int(eancode[ean13_len-1]): # last digit
-         return False
-     return True
+     res = ean_checksum(eancode) == int(eancode[-1])
+     if not res and len(eancode)==8:
+         res = ean_checksum(convert_UPCE_to_UPCA(eancode)) == int(eancode[-1])
+     return res
+# tests (should return empty list):
+# [ (name, code) for name, code in (('upc-e','04904500'),('ean-13','2112345678900'),('ean-8','02345673'),('upc-a','416000336108'),('upc-e','00123457'),) if not check_ean(code)]
 
-# need to replace the check_ean13_key function 
+def ean_checksum(eancode):
+    # eancode -- code without check digit
+    oddsum=0
+    evensum=0
+    total=0
+    eanvalue=eancode
+    reversevalue = eanvalue[::-1]
+    finalean=reversevalue[1:]
+    for i in range(len(finalean)):
+        if i % 2 == 0:
+            oddsum += int(finalean[i])
+        else:
+            evensum += int(finalean[i])
+    total=(oddsum * 3) + evensum
+    check = int(10 - math.ceil(total % 10.0)) %10
+    return check
+
+# from http://code.activestate.com/recipes/528911-barcodes-convert-upc-e-to-upc-a/
+def convert_UPCE_to_UPCA(upce_value):
+    """Test value 04182635 -> 041800000265"""
+    if len(upce_value)==6:
+        middle_digits=upce_value #assume we're getting just middle 6 digits
+    elif len(upce_value)==7:
+        #truncate last digit, assume it is just check digit
+        middle_digits=upce_value[:6]
+    elif len(upce_value)==8:
+        #truncate first and last digit,
+        #assume first digit is number system digit
+        #last digit is check digit
+        middle_digits=upce_value[1:7]
+    else:
+        return False
+    d1,d2,d3,d4,d5,d6=list(middle_digits)
+    if d6 in ["0","1","2"]:
+        mfrnum=d1+d2+d6+"00"
+        itemnum="00"+d3+d4+d5
+    elif d6=="3":
+        mfrnum=d1+d2+d3+"00"
+        itemnum="000"+d4+d5
+    elif d6=="4":
+        mfrnum=d1+d2+d3+d4+"0"
+        itemnum="0000"+d5
+    else:
+        mfrnum=d1+d2+d3+d4+d5
+        itemnum="0000"+d6
+    newmsg="0"+mfrnum+itemnum
+    #calculate check digit, they are the same for both UPCA and UPCE
+    check_digit=ean_checksum(newmsg+'0')
+    return newmsg+str(check_digit)
+
+
+# need to replace the check_ean13_key function
 class product_product(osv.osv):
     _inherit = "product.product"
 
@@ -59,12 +99,27 @@ class product_product(osv.osv):
         return res
 
     _columns = {
-        'ean13': fields.char('EAN', help ='Code for EAN8 EAN13 UPC JPC GTIN http://de.wikipedia.org/wiki/Global_Trade_Item_Number', size=14),
+        'ean13': fields.char('Barcode', help ='Code for EAN8 EAN13 UPC JPC GTIN http://de.wikipedia.org/wiki/Global_Trade_Item_Number', size=14),
     }
 
-    _constraints = [(_check_ean_key, 'Error: Invalid EAN code', ['ean13'])]
+    _constraints = [(_check_ean_key, 'Error: Invalid barcode', ['ean13'])]
 
 product_product()
+
+class product_template(osv.osv):
+    _inherit = "product.template"
+
+    def _check_ean_key(self, cr, uid, ids, context=None):
+        for rec in self.browse(cr, uid, ids, context=context):
+            res = check_ean(rec.ean13)
+        return res
+
+    _columns = {
+        'ean13': fields.related('product_variant_ids', 'ean13', type='char', string='Barcode'),
+
+    }
+
+product_template()
 
 
 class product_packaging(osv.osv):
@@ -76,9 +131,9 @@ class product_packaging(osv.osv):
         return res
 
     _columns = {
-        'ean':    fields.char('EAN', help ='Barcode number for EAN8 EAN13 UPC JPC GTIN', size=14),
+        'ean':    fields.char('Barcode', help ='Barcode number for EAN8 EAN13 UPC JPC GTIN', size=14),
     }
-    _constraints = [(_check_ean_key, 'Error: Invalid EAN code', ['ean'])]
+    _constraints = [(_check_ean_key, 'Error: Invalid barcode', ['ean'])]
 
 product_packaging()
 
@@ -92,10 +147,10 @@ class res_partner(osv.osv):
         return res
 
     _columns = {
-        'ean13':    fields.char('EAN', help ='Code for EAN8 EAN13 UPC JPC GTIN http://de.wikipedia.org/wiki/Global_Trade_Item_Number', size=14),
+        'ean13':    fields.char('Barcode', help ='Code for EAN8 EAN13 UPC JPC GTIN http://de.wikipedia.org/wiki/Global_Trade_Item_Number', size=14),
     }
 
-    _constraints = [(_check_ean_key, 'Error: Invalid EAN code', ['ean13'])]
+    _constraints = [(_check_ean_key, 'Error: Invalid barcode', ['ean13'])]
 
 res_partner()
 
