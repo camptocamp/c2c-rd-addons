19a0962bf9538de86368b5f54aaa73cca1a66bcc
Ferdinand Gassauer <f.gassauer@chricar.at>
2015-05-19 11:34:16 +0200

    [FIX] wrong code was installed

--- chricar_sale_farm/sale.py
+++ chricar_sale_farm/sale.py
@@ -39,7 +39,8 @@ class sale_order_line(osv.osv):
     _inherit = "sale.order.line"
 #    _logger = logging.getLogger(_name)
 
-    def _invoiced_qty(self, cursor, user, ids, name, arg, context=None):
+    #def _invoiced_qty(self, cursor, user, ids, name, arg, context=None):
+    def _invoiced_qty(self, cursor, user, ids, context=None):
         res = {}
         for line in self.browse(cursor, user, ids, context=context):
             res[line.id] = 0
@@ -50,7 +51,8 @@ class sale_order_line(osv.osv):
                             res[line.id] += inv_line.quantity
         return res
 
-    def _invoiced_amount(self, cursor, user, ids, name, arg, context=None):
+    #def _invoiced_amount(self, cursor, user, ids, name, arg, context=None):
+    def _invoiced_amount(self, cursor, user, ids, context=None):
         res = {}
         for line in self.browse(cursor, user, ids, context=context):
             res[line.id] = 0
@@ -58,10 +60,14 @@ class sale_order_line(osv.osv):
                 for inv in line.order_id.invoice_ids:
                     for inv_line in inv.invoice_line:
                         if inv_line.product_id == line.product_id:
-                            res[line.id] += inv_line.price_subtotal
+                            if inv_line.invoice_id.type == 'out_invoice':
+                                res[line.id] += inv_line.price_subtotal
+                            else: 
+                                res[line.id] -= inv_line.price_subtotal
         return res
 
-    def _invoiced_price(self, cursor, user, ids, name, arg, context=None):
+    #def _invoiced_price(self, cursor, user, ids, name, arg, context=None):
+    def _invoiced_price(self, cursor, user, ids, context=None):
         res = {}
         for line in self.browse(cursor, user, ids, context=context):
             if line.invoiced_qty != 0:
@@ -70,18 +76,23 @@ class sale_order_line(osv.osv):
                 res[line.id] = 0
         return res
 
-    def _delivered_qty(self, cursor, user, ids, name, arg, context=None):
+    #def _delivered_qty(self, cursor, user, ids, name, arg, context=None):
+    def _delivered_qty(self, cursor, user, ids, context=None):
         res = {}
         for line in self.browse(cursor, user, ids, context=context):
             res[line.id] = 0
             if line.order_id.picking_ids:
                  for pick in line.order_id.picking_ids:
                     for pick_line in pick.move_lines:
-                        if pick_line.product_id.id == line.product_id.id and pick_line.location_dest_id.usage in['inventory', 'customer'] and pick_line.state == 'done':
-                            res[line.id] += pick_line.product_qty
+                        if pick_line.product_id.id == line.product_id.id and pick_line.state == 'done':
+                            if pick_line.location_dest_id.usage in['inventory', 'customer']:
+                                res[line.id] += pick_line.product_qty
+                            if pick_line.location_id.usage in['inventory', 'customer']:
+                                res[line.id] -= pick_line.product_qty
         return res
 
-    def _diff_qty(self, cursor, user, ids, name, arg, context=None):
+    #def _diff_qty(self, cursor, user, ids, name, arg, context=None):
+    def _diff_qty(self, cursor, user, ids, context=None):
         res = {}
         for line in self.browse(cursor, user, ids, context=context):
             if line.invoiced_qty - line.delivered_qty < 0:
@@ -90,7 +101,8 @@ class sale_order_line(osv.osv):
                 res[line.id] = 0
         return res
 
-    def _diff_percent(self, cursor, user, ids, name, arg, context=None):
+    #def _diff_percent(self, cursor, user, ids, name, arg, context=None):
+    def _diff_percent(self, cursor, user, ids, context=None):
         res = {}
         for line in self.browse(cursor, user, ids, context=context):
             if  line.delivered_qty > 0 and line.diff_qty < 0:
@@ -99,7 +111,8 @@ class sale_order_line(osv.osv):
                 res[line.id] = 0
         return res
 
-    def _uninvoiced_amount(self, cursor, user, ids, name, arg, context=None):
+    #def _uninvoiced_amount(self, cursor, user, ids, name, arg, context=None):
+    def _uninvoiced_amount(self, cursor, user, ids, context=None):
         res = {}
         for line in self.browse(cursor, user, ids, context=context):
             res[line.id] = line.invoiced_price  * line.diff_qty
@@ -107,19 +120,22 @@ class sale_order_line(osv.osv):
 
     _columns = {
          'delivered_qty' : fields.function(_delivered_qty, string='Delivered QTY', type='float',
-                           store={'stock.move': (_delivered_qty, ['product_qty'], 10) }),
+                           #store={'stock.move': (_delivered_qty, ['product_qty'], 10) }),
+                           store={'sale.order.line'    : (lambda self, cr, uid, ids, c={}: ids, ['delivered_qty'], 10),}),
          'invoiced_qty'  : fields.function(_invoiced_qty, string='Invoiced QTY', type='float',
-                           store={'account.invoice.line': ( _invoiced_qty, ['quantity'], 10) }),
+                           #store={'account.invoice.line': ( _invoiced_qty, ['quantity'], 10) }),
+                           store={'sale.order.line'    : (lambda self, cr, uid, ids, c={}: ids, ['invoiced_qty'], 10),}),
          'diff_qty'      : fields.function(_diff_qty, string='Waste QTY', type='float', 
-                           store={ 'stock.move': (_diff_qty, ['product_qty'], 10),
-                                   'account.invoice.line': ( _diff_qty, ['quantity'], 10)
-                                }),
+                           #store={ 'stock.move': (_diff_qty, ['product_qty'], 10),
+                           #        'account.invoice.line': ( _diff_qty, ['quantity'], 10)
+                           store={'sale.order.line'    : (lambda self, cr, uid, ids, c={}: ids, ['diff_qty'], 10), }),
          'diff_percent'  : fields.function(_diff_percent, string='Diff %', type='float'),
          'invoiced_amount':fields.function(_invoiced_amount, string='Invoiced Amount', type='float',
-                           store={'account.invoice.line': ( _invoiced_amount, ['price_subtotal'], 10) }), 
+                           #store={'account.invoice.line': ( _invoiced_amount, ['price_subtotal'], 10) }), 
+                           store={'sale.order.line'    : (lambda self, cr, uid, ids, c={}: ids, ['invoiced_amount'], 10),}),
          'uninvoiced_amount':fields.function(_uninvoiced_amount, string='Waste Value', type='float',
-                           store= {'sale.order.line'    : (lambda self, cr, uid, ids, c={}: ids, ['diff_qty'], 10),
-                                  'account.invoice.line': ( _uninvoiced_amount, ['price_subtotal'], 10)
+                           store= {'sale.order.line'    : (lambda self, cr, uid, ids, c={}: ids, ['uninvoiced_amount'], 10),
+                                  #'account.invoice.line': ( _uninvoiced_amount, ['price_subtotal'], 10)
                                   }),
          'invoiced_price': fields.function(_invoiced_price, string='Invoiced Price', type='float'),
          'date_order'    : fields.related('order_id', 'date_order', type='date', string='Date Order', store=True),
